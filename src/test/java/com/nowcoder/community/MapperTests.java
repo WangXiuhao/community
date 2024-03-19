c98b912e4b2fb3b6a9c2ee51415a640e3395f8cb
WangXiuhao <wangxiuhao666@126.com>
2024-03-19 09:51:49 +0800

    增加数据库的查询 更新

--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 /**
  * @author 王修豪
  * @version 1.0
@@ -21,14 +23,44 @@ public class MapperTests {
     @Autowired
     private UserMapper userMapper;
 
+
+    //需要测试001
     @Test
     public void testSelectUser(){
         User user = userMapper.selectById(101);
         System.out.println(user);
-//        user = userMapper.selectByName("liubei");
-
-        user=userMapper.selectById(101);
-
+        user = userMapper.selectByName("liubei");
         System.out.println(user);
+        user=userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    //需要测试002
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@126.com");
+        user.setHeaderUrl("https://www.leodeluntan.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    //需要测试003
+    @Test
+    public void updateUser(){
+        int rows = userMapper.updateStates(101,0);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(101,"https://www.leodeluntan.com/101.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(101,"5277");
+        System.out.println(rows);
     }
 }
