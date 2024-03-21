456e73d733ab1f9513bdd4d87812db7f2ddaa36e
WangXiuhao <wangxiuhao666@126.com>
2024-03-22 01:02:02 +0800

    前端好像有点问题

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
 import com.nowcoder.community.sevice.DiscussPostService;
 import com.nowcoder.community.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author 王修豪
@@ -22,7 +29,20 @@ public class HomeController {
 
     @RequestMapping(path = "/index",method = RequestMethod.GET)
     public String getIndexPage(Model model){//model携带数据
-//41:12
-         return "/index";
+        //查询 先查询前十条数据
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 0, 10);
+        //要通过id得到name
+        List<Map<String,Object>> discussPosts = new ArrayList<>();
+        if(list != null){
+            for(DiscussPost post:list){
+                Map<String,Object> map = new HashMap<>();
+                map.put("post",post);//把帖子放进去
+                User user = userService.findUserById(post.getUserId());
+                map.put("user",user);//放入用户
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts",discussPosts);
+        return "index";
     }
 }
