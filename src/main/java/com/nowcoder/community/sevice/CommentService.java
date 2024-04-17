7c9d86c1a3784a3105209b1a2f580837fbbee17a
WangXiuhao <wangxiuhao666@126.com>
2024-04-17 14:40:17 +0800

    显示评论业务完成

---
+++ src/main/java/com/nowcoder/community/sevice/CommentService.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.sevice;
+
+import com.nowcoder.community.dao.CommentMapper;
+import com.nowcoder.community.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author 王修豪
+ * @version 1.0
+ */
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentMapper commentMapper;
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
+    }
+
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+}
