338f330daba9f8ed5b1032ee77a7e801ecbd89c0
WangXiuhao <wangxiuhao666@126.com>
2024-04-21 15:59:51 +0800

    收到的赞的功能完成

--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -2,7 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,14 +18,35 @@ public class LikeService {
     private RedisTemplate redisTemplate;
 
     // 点赞 如果点赞过就取消赞 没赞过就点赞
-    public void like(int userId,int entityType,int entityId){
-        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType,entityId);
-        Boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
-        if(isMember){
-            redisTemplate.opsForSet().remove(entityLikeKey,userId);
-        }else{
-            redisTemplate.opsForSet().add(entityLikeKey,userId);
-        }
+    public void like(int userId,int entityType,int entityId,int entityUserId){
+//        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType,entityId);
+//        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+//        if(isMember){
+//            redisTemplate.opsForSet().remove(entityLikeKey,userId);
+//        }else{
+//            redisTemplate.opsForSet().add(entityLikeKey,userId);
+//        }
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType,entityId);
+                String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
+
+                boolean isMember = operations.opsForSet().isMember(entityLikeKey,userId);//判断这个用户有没有点过赞
+
+                operations.multi();
+
+                if(isMember){
+                    operations.opsForSet().remove(entityLikeKey,userId);
+                    operations.opsForValue().decrement(userLikeKey);
+                }else{
+                    operations.opsForSet().add(entityLikeKey,userId);
+                    operations.opsForValue().increment(userLikeKey);
+                }
+
+                return operations.exec();
+            }
+        });
     }
     //查询实体点赞的数量
     public long findEntityLikeCount(int entityType,int entityId){
@@ -34,6 +58,14 @@ public class LikeService {
         String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType,entityId);
         return redisTemplate.opsForSet().isMember(entityLikeKey,userId) ? 1:0;
     }
+    //查询某个用户获得的赞的数量
+    public int findUserLikeCount(int userId){
+        String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
+        Integer count = (Integer) redisTemplate.opsForValue().get(userLikeKey);
+        return count == null ? 0:count.intValue();//有可能是null
+
+    }
+
 
     
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -6,11 +6,17 @@ package com.nowcoder.community.util;
  */
 public class RedisKeyUtil {
     private static final String SPLIT=":";//以冒号来分割各种单词
-    private static final String PREFIX_ENTITY_LIKE="like:entity";//前缀
+    private static final String PREFIX_ENTITY_LIKE="like:entity";//实体前缀
+    private static final String PREFIX_USER_LIKE = "like:user";//用户前缀
     //某个实体的赞
     // like:entity:entityType:entityId -> set(userId)存点赞的id
     public static String getEntityLikeKey(int entityType,int entityId){
         return PREFIX_ENTITY_LIKE+SPLIT+entityType+SPLIT+entityId;
+    }
+    //某个用户的赞
+//    like:user:userId ->int
+    public static String getUserLikeKey(int userid){
+        return PREFIX_USER_LIKE+SPLIT+userid;
     }
 
 }
